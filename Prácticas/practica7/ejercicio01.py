6789f3ab103d1276b8cd62d1ee18e6b970bded3c
guillemfustier <al426661@uji.es>
2025-05-24 11:57:02 +0200

    p7 terminada

--- Prácticas/practica7/ejercicio01.py
+++ Prácticas/practica7/ejercicio01.py
@@ -15,31 +15,36 @@ for img in imgs_monedas:
     img_umbralizada = img > umbral_otsu  # Objetos más oscuros que el fondo
     print(f"Umbral seleccionado (Otsu): {umbral_otsu}")
 
-    st_elem = ski.morphology.disk(8)
+    st_elem = ski.morphology.disk(2)
     
-    if i != 2:
-        img_apertura = ski.morphology.binary_closing(img_umbralizada, footprint=st_elem)
-    else:
-        st_elem1 = None
-        st_elem2 = ski.morphology.disk(1)
-        img_erosion = ski.morphology.erosion(img_umbralizada, footprint=st_elem1)
-        img_apertura = ski.morphology.binary_dilation(img_erosion, footprint=st_elem2)
+    img_cierre = ski.morphology.binary_closing(img_umbralizada, footprint=st_elem)
 
-    img_etiquetada = ski.morphology.label(img_apertura)
+    st_elem1 = ski.morphology.disk(11)
+    st_elem2 = ski.morphology.disk(7)
+    img_erosion = ski.morphology.binary_erosion(img_cierre, footprint=st_elem1)
+    img_cierre = ski.morphology.binary_dilation(img_erosion, footprint=st_elem2)
+
+    img_etiquetada = ski.morphology.label(img_cierre)
 
     props = ski.measure.regionprops(img_etiquetada)
     for p in props:
         print(f"Etiqueta: {p.label} Área: {p.area} Excentricidad: {p.eccentricity:.2f}")
 
     resultado = ski.color.gray2rgb(img*0)
-    contador_monedas = 0
+    contador_monedas_euro = 0
+    contador_monedas_cent = 0
     ecc_list = [(0.10, 0.07), (0.20, 0.13)]
     for p in props:
-        if p.eccentricity < 0.85:
-            resultado[img_etiquetada == p.label] = (255, 0, 0)
-            contador_monedas += 1
-    print(f"Detectadas {contador_monedas} monedas")
-    imgs_resultados.append(img_etiquetada)
+        if p.eccentricity < 0.50:
+            if p.area < 750:
+                resultado[img_etiquetada == p.label] = (0, 255, 0)
+                contador_monedas_cent += 1
+            else:
+                resultado[img_etiquetada == p.label] = (255, 0, 0)
+                contador_monedas_euro += 1
+    print(f"Detectadas {contador_monedas_euro} monedas de 1 euro")
+    print(f"Detectadas {contador_monedas_cent} monedas de 10 céntimos")
+    imgs_resultados.append(resultado)
     i += 1
 
 # Visualizar resultados
@@ -49,9 +54,9 @@ for ax in axs.ravel():
 
 for i in range(3):
     axs[0, i].imshow(imgs_monedas[i], cmap="gray")
-    axs[0, i].set_title(f"Original {i}", fontsize=16)
+    axs[0, i].set_title(f"Original {i+1}", fontsize=16)
 
     axs[1, i].imshow(imgs_resultados[i], cmap="gray")
-    axs[1, i].set_title("Cierre", fontsize=16)
+    axs[1, i].set_title("Monedas detectadas", fontsize=16)
 
 plt.show()
